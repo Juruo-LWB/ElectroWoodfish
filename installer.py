fa7f28139358369e71ce83d3c871d44145dac5e9
Juruo-LWB <152617862+Juruo-LWB@users.noreply.github.com>
2024-01-05 20:37:45 +0800

    Create installer.py
    Installer V1.0

---
+++ installer.py
@@ -0,0 +1,25 @@
+"""
+	ElectronWoodFish->install.py
+    (C)2024 LWB
+    This file is an installer.
+    Last modified:24/1/5
+    V1.0
+"""
+
+import tkinter as tk
+import tkinter.messagebox as msg
+import os
+
+"""
+define some variable
+"""
+installPath=""
+"""
+Path to create shortcut
+"""
+shortcutPath="\"%appdata%\\Microsoft\\Windows\\Start Menu\\Programs\\LWB softs\""
+
+
+"""
+Prepare folders we need
+"""
